75aa77589036570b665628e71942a59d309b0b79
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2022-04-19 19:17:08 +0200

    Implemented pump control
    Signed-off-by: Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>

--- ESP32/lib/pump/pump.h
+++ ESP32/lib/pump/pump.h
@@ -7,19 +7,27 @@
 #ifndef ESP32_PUMP_H
 #define ESP32_PUMP_H
 
+// 1/2 ml/s = 1/2 * 10^-3 ml/ms
+#define PUMP_SPEED 0.0005f
+
+// formic acid has a density of 1.22 g/ml
+#define FORMIC_ACID_DENSITY 1.22f
 
 class Pump {
 private:
     PinIO *pinIo;
+    bool pumping;
+    int pin = -1;
+    long startTime = 0;
 
 public:
-    Pump(PinIO *pinIo) {
-        this->pinIo = pinIo;
-    }
-
-    int pumpMilliliters(int deltaTime, float milliliter) {
-        return 0;
-    }
+    Pump(PinIO *pinIo, int pin);
+    // 1/2 ml / s
+    void start(long startTime);
+    void stop();
+    float pumpedMilliliter(long currentTime);
+    float pumpedGrams(long currentTime);
+    bool isPumping();    
 };
 
 
--- ESP32/test/test_desktop/test_pump.cpp
+++ ESP32/test/test_desktop/test_pump.cpp
@@ -14,16 +14,53 @@ public:
     MockPinIO() {}
 
     void on(int pin) {
+        this->map[pin] = true;
         std::cout << "on: " << pin << std::endl;
     }
-    void off(int pin) {}
-    void toggle(int pin) {}
+    void off(int pin) {
+        this->map[pin] = false;
+        std::cout << "off: " << pin << std::endl;
+    }
+    void toggle(int pin) {
+        if (this->map.find(pin) == this->map.end()) {
+            this->map[pin] = false;
+        }
+
+        if (this->map[pin]) {
+            this->off(pin);
+        } else {
+            this->on(pin);
+        }
+    }
 };
 
 void test_pump(void) {
-    PinIO *mockPinIo = new MockPinIO();
-    Pump pump = Pump(new MockPinIO());
-    mockPinIo->on(1);
+    int testPumpPin = 5;
+    float startTime = 0;
+    float currentTime = 0;
+    float milliliters = 10;
+
+    MockPinIO *mockPinIo = new MockPinIO();
+    Pump *pump = new Pump(mockPinIo, testPumpPin);
+
+    TEST_ASSERT(!mockPinIo->map[testPumpPin])
+
+    pump->start(0);
+
+    TEST_ASSERT(mockPinIo->map[testPumpPin])
+
+    // Speed is 1/2 ml / s so double the time
+    currentTime = 2 * milliliters * 1000;
+    float pumpedMilliliter = pump->pumpedMilliliter(currentTime);
+    float pumpedGrams = pump->pumpedGrams(currentTime);
+
+    TEST_ASSERT(pumpedMilliliter >= milliliters && pumpedMilliliter < milliliters + 0.1f);
+    TEST_ASSERT(pumpedGrams >= pumpedMilliliter * FORMIC_ACID_DENSITY && pumpedGrams <= pumpedMilliliter * FORMIC_ACID_DENSITY + 0.1f);
+    TEST_ASSERT(pump->isPumping());
+
+    pump->stop();
+    TEST_ASSERT(!mockPinIo->map[testPumpPin])
+    TEST_ASSERT(!pump->isPumping());
 }
 
 int main(int argc, char **argv) {
